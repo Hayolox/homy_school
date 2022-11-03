c56c855c650894ddce339311186ec79f35a50583
Hayolox <akiklii72@gmail.com>
2022-11-03 17:32:52 +0700

    crud class activies

--- lib/views/model_view/class_activities_view_model.dart
+++ lib/views/model_view/class_activities_view_model.dart
@@ -27,23 +27,30 @@ class ClassActivitiesViewModel extends ChangeNotifier {
       String paramName, String paramDescription, BuildContext context) async {
     CollectionReference dataClassActivities = db.collection('class-activities');
 
-    if (paramName.isNotEmpty || paramDescription.isNotEmpty) {
-      await dataClassActivities.add(
-        {
-          'description': paramDescription,
-          'name': paramName,
-        },
-      );
-      // ignore: use_build_context_synchronously
-      FutureDialog()
-          .alert(context, 'Succes!', 'Berhasil Menambahkan Kegiatan Kelas');
+    if (paramName.isNotEmpty && paramDescription.isNotEmpty) {
+      bool isNameUniq = await isDuplicateUniqueName(paramName);
+
+      if (isNameUniq != true) {
+        await dataClassActivities.add(
+          {
+            'description': paramDescription,
+            'name': paramName,
+          },
+        );
+        nameActivitiesC.clear();
+        desctiprionActivitiesC.clear();
+        // ignore: use_build_context_synchronously
+        FutureDialog()
+            .alert(context, 'Succes!', 'Berhasil Menambahkan Kegiatan Kelas');
+      } else {
+        // ignore: use_build_context_synchronously
+        FutureDialog()
+            .alert(context, 'Info!', 'Name Sudah ada, name harus uniq');
+      }
     } else {
       FutureDialog()
           .alert(context, 'Info!', 'Name atau Description Tidak Boleh kosong');
     }
-
-    nameActivitiesC.clear();
-    desctiprionActivitiesC.clear();
   }
 
   editData(String paramName, String paramDescription, String paramId,
@@ -51,23 +58,56 @@ class ClassActivitiesViewModel extends ChangeNotifier {
     DocumentReference dataClassActivities =
         db.collection('class-activities').doc(paramId);
 
-    if (paramName.isNotEmpty || paramDescription.isNotEmpty) {
-      await dataClassActivities.update(
-        {
-          'description': paramDescription,
-          'name': paramName,
+    if (paramName.isNotEmpty && paramDescription.isNotEmpty) {
+      FirebaseFirestore.instance
+          .collection('class-activities')
+          .doc(paramId)
+          .get()
+          .then(
+        (value) async {
+          if ((value.data() as Map<String, dynamic>)['name'] == paramName) {
+            dataClassActivities.update(
+              {
+                'description': paramDescription,
+              },
+            );
+            nameActivitiesC.clear();
+            desctiprionActivitiesC.clear();
+            // ignore: use_build_context_synchronously
+            FutureDialog()
+                .alert(context, 'Succes!', 'Berhasil Mengedit Kegiatan Kelas');
+
+            // ignore: use_build_context_synchronously
+            Navigator.pop(context);
+            // ignore: use_build_context_synchronously
+            Navigator.pop(context);
+          } else {
+            bool isNameUniq = await isDuplicateUniqueName(paramName);
+            if (isNameUniq != true) {
+              dataClassActivities.update(
+                {
+                  'description': paramDescription,
+                  'name': paramName,
+                },
+              );
+              nameActivitiesC.clear();
+              desctiprionActivitiesC.clear();
+              // ignore: use_build_context_synchronously
+              FutureDialog().alert(
+                  context, 'Succes!', 'Berhasil Mengedit Kegiatan Kelas');
+
+              // ignore: use_build_context_synchronously
+              Navigator.pop(context);
+              // ignore: use_build_context_synchronously
+              Navigator.pop(context);
+            } else {
+              // ignore: use_build_context_synchronously
+              FutureDialog()
+                  .alert(context, 'Info!', 'Name Sudah ada, name harus uniq');
+            }
+          }
         },
       );
-
-      nameActivitiesC.clear();
-      desctiprionActivitiesC.clear();
-      // ignore: use_build_context_synchronously
-      FutureDialog()
-          .alert(context, 'Succes!', 'Berhasil Mengedit Kegiatan Kelas');
-      // ignore: use_build_context_synchronously
-      Navigator.pop(context);
-      // ignore: use_build_context_synchronously
-      Navigator.pop(context);
     } else {
       FutureDialog()
           .alert(context, 'Info!', 'Name atau Description Tidak Boleh kosong');
@@ -77,5 +117,13 @@ class ClassActivitiesViewModel extends ChangeNotifier {
   void deleteData(String paramIdDoc) async {
     await db.collection("class-activities").doc(paramIdDoc).delete();
     notifyListeners();
+  }
+
+  Future<bool> isDuplicateUniqueName(String uniqueName) async {
+    QuerySnapshot query = await db
+        .collection('class-activities')
+        .where('name', isEqualTo: uniqueName)
+        .get();
+    return query.docs.isNotEmpty;
   }
 }
